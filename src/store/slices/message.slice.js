e2051eee9211d431bb56e65961ab2469d76942c4
BrianMacia <brianmacia@hotmail.com>
2023-02-20 13:03:23 -0300

    mail

--- src/store/slices/message.slice.js
+++ src/store/slices/message.slice.js
@@ -2,6 +2,7 @@ import { createSlice } from "@reduxjs/toolkit";
 
 export const messageSlice = createSlice({
   name: "message",
+  email: "email",
   initialState: null,
   reducers: {
     setMessage: (state, action) => action.payload,
